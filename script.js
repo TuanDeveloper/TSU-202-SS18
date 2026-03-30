6ecc118b695480892a508b5cb3924608c62016c3
Tuancoder2002 <it.coder.quoctuan@gmail.com>
2026-03-30 20:24:43 +0700

    array method

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-// Global
-let a = 10;
-let b = 20;
-
-// Khai báo hàm,
-// tham số là biến được truyền vào dùng để lưu trữ giá trị tạm thời khi khai báo hàm
-//Function declaration
-function sumNumber(a, b) {
-    // code here
-    let result = a + b;
-    return result;
-    console.log(result + 5);
-}
-console.log(sumNumber(20, 20));
-
-
-//Function expression
-
-// const sum = function (a, b) {
-//     let result = a + b;
-//     return result;
+let numbers = [4, 2, 6, 12, 8];
+// Truyen thong
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
 // }
-// console.log(sum(35, 20));
+// console.log(sum);
 
-// Arrow function
-const sum = (a, b) => a + b;
-console.log(sum(35, 20));
 
-function isEven(num) {
-    if (num % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-let num = Number(prompt('Nhập 1 số nguyên :'));
-alert('Kết quả : ' + isEven(num));
+// forEach Duyệt qua từng phân tử của mảng
+// numbers.forEach(function (element, index) {
+//     console.log(element * 2);
+// })
+
+
+// map Duyệt qua mảng và xử lý các phần tử và trả về mảng mới dựa trên các phần tử đó
+// let newArray = numbers.map(function(element, index) {
+//     return element - 1;
+// })
+
+// console.log(newArray);
+
+//filter lọc qua mảng cũ và lấy những phần tử thỏa mãn điều kiện thêm vào mảng mới
+// let newArray = numbers.filter(function(element, index) {
+//     return element % 2 == 0;
+// })
+
+// let check = numbers.some(function (element, index) {
+//     return element % 2 == 0;
+// })
+
+// let check2 = numbers.every(function (element) {
+//     return element % 2 == 0;
+// })
+
+// console.log(check2);
+
+
+//reduce Dùng để tính giá trị tích lũy theo từng vòng lặp của mảng
+let result = numbers.reduce((acc, curr) => acc + curr, 0)
+
+console.log(result);
+
+
+
+
 
